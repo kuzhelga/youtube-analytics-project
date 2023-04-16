2b1c568e557e6c1cb2404bcc229ad112fdab9124
kuzhelga <kuzhelga81@gmail.com>
2023-04-16 18:28:35 +0300

    added channel_id getter

--- src/channel.py
+++ src/channel.py
@@ -11,18 +11,23 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
-        channel = youtube.channels().list(id=self.channel_id, part="snippet,statistics").execute()
+        self.__channel_id = channel_id
+        channel = youtube.channels().list(id=self.__channel_id, part="snippet,statistics").execute()
         self.title = channel["items"][0]["snippet"]["title"]
         self.description = channel["items"][0]["snippet"]["description"]
-        self.url = f"https://www.youtube.com/channel/{self.channel_id}"
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
         self.subscriber_count = channel["items"][0]["statistics"]["subscriberCount"]
         self.video_count = channel["items"][0]["statistics"]["videoCount"]
         self.view_count = channel["items"][0]["statistics"]["viewCount"]
 
+    @property
+    def channel_id(self):
+        """Геттер для запрета изменения вывода ID канала"""
+        return self.__channel_id
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part="snippet,statistics").execute()
+        channel = youtube.channels().list(id=self.__channel_id, part="snippet,statistics").execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
 
     @classmethod
@@ -30,3 +35,18 @@ class Channel:
         """Возвращает объект для работы с YouTube API"""
         youtube_object = build('youtube', 'v3', developerKey=api_key)
         return youtube_object
+
+    def to_json(self, filename):
+        """Сохраняет в файл значения атрибутов экземпляра"""
+        attributes = {
+        'channel_id': self.__channel_id,
+        'title': self.title,
+        'description': self.description,
+        'url': self.url,
+        'subscriber_count': self.subscriber_count,
+        'video_count': self.video_count,
+        'view_count': self.view_count
+        }
+
+        with open(filename, 'w') as file:
+            json.dump(attributes, file, ensure_ascii=False, indent="")
