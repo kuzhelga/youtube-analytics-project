f6633e4af8d97b2bbe28df006c958a7b929038d9
kuzhelga <kuzhelga81@gmail.com>
2023-04-16 17:31:06 +0300

    added attributes

--- src/channel.py
+++ src/channel.py
@@ -2,7 +2,6 @@ import json
 import os
 from googleapiclient.discovery import build
 
-
 api_key: str = os.getenv('API_KEY')  # подтягивается API_KEY из системы
 youtube = build('youtube', 'v3', developerKey=api_key)  # создается специальный объект для работы с API
 
@@ -13,6 +12,13 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        channel = youtube.channels().list(id=self.channel_id, part="snippet,statistics").execute()
+        self.title = channel["items"][0]["snippet"]["title"]
+        self.description = channel["items"][0]["snippet"]["description"]
+        self.url = f"https://www.youtube.com/channel/{self.channel_id}"
+        self.subscriber_count = channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = channel["items"][0]["statistics"]["viewCount"]
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
